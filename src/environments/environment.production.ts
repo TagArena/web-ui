24860dbcbd2729318d0e4379257e17ac80e7788f
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2025-06-23 15:06:00 +0200

    Delete src directory

--- src/environments/environment.production.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  baseDomain: "roboter5123.com",
-  httpProtocol: "https",
-  servicePrefix: ""
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  baseDomain: "",
-  httpProtocol: "",
-  servicePrefix: ""
-};
