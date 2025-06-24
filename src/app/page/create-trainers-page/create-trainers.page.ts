8e409fc5f1a993f31e0f351eeeaf5c60a03939e5
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2025-06-24 07:43:01 +0200

    Update create-trainers.page.ts

--- src/app/page/create-trainers-page/create-trainers.page.ts
+++ src/app/page/create-trainers-page/create-trainers.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {CreateTrainerComponent} from "../../component/trainer-creator/create-trainer/create-trainer.component";
 
 @Component({
   selector: 'app-create-trainers.page',
-  imports: [],
+  imports: [CreateTrainerComponent],
   templateUrl: './create-trainers.page.html',
   standalone: true,
   styleUrl: './create-trainers.page.css'
